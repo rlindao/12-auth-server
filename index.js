5ee5a0b25b457dc5e40cfa067ad644646a84149f
Roberto Andres Lindao Guevara <lindaorobertoandres@gmail.com>
2021-11-13 16:09:20 +0000

    Init de node

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+//Crear el servidor/aplicación de express
+const app= express();
+
+
+//GET
+app.get('/' , (req , res) => {
+        
+    res.json({
+        ok:     true,
+        msg:    'Todo salio bien',
+        uid:    1234
+    })
+})
+
+app.listen( 4000 , () => {
+    console.log(`Servidor corriendo en puerto ${4000} `);
+} )
